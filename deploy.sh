b6e7f5f5f1de5661a0113464c66c03dab137f37b
laposmokus <you@example.com>
2019-01-11 14:09:43 +0100

    google cloud

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push laposmokus/multi-worker:$SHA
 kubectl apply -f k8s
 
 kubectl set image deployments/server-deployment server=laposmokus/multi-server:$SHA
-kubectl set image deployments/client-deployment server=laposmokus/client-server:$SHA
-kubectl set image deployments/worker-deployment server=laposmokus/worker-server:$SHA
+kubectl set image deployments/client-deployment server=laposmokus/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=laposmokus/multi-worker:$SHA
